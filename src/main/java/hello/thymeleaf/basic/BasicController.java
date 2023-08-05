4e43eeb5106a8febb0af6162db0aad1fa59c5848
sion <shtldhs123@naver.com>
2023-08-05 21:30:41 +0900

    ✨ Feat: Thymeleaf SpringEL 오브젝트, 리스트, 맵 접근 및 지역 변수 사용

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package hello.thymeleaf.basic;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/basic")
 @Controller
@@ -19,5 +25,44 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = User.builder()
+                .username("userA")
+                .age(10)
+                .build();
+        User userB = User.builder()
+                .username("userB")
+                .age(20)
+                .build();
+
+        List<User> users = List.of(userA, userB);
+
+        Map<String, User> userMap = Map.of(
+                "userA", userA,
+                "userB", userB
+        );
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", users);
+        model.addAttribute("userMap", userMap);
+
+        return "basic/variable";
+    }
+
+    @Getter
+    static class User {
+        @JsonProperty(value = "username")
+        private String username;
+        @JsonProperty(value = "age")
+        private int age;
+
+        @Builder
+        private User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
